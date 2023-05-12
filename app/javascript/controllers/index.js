31278d3653ca842d1a61ebac498d86953cd04d35
Andrew Dembiczak <andrew.dembiczak@gmail.com>
2023-05-12 15:08:29 +0900

    Removed js controllers

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,9 +3,3 @@
 // ./bin/rails generate stimulus controllerName
 
 import { application } from "./application"
-
-import TomSelectController from "./tom_select_controller.js"
-application.register("tom-select", TomSelectController)
-
-import StarRatingController from "./star_rating_controller.js"
-application.register("star-rating", StarRatingController)
